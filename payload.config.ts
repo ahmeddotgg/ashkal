80520a05d9b7d0ad5a74341d6f121a796f7fd8d4
Ahmed Said <ahmeddsaidd@proton.me>
2025-04-20 12:23:31 +0200

    Refactor and clean up Payload configuration
    Removed unused files and collections, simplified Payload configuration, and updated dependencies to use exact versions. This includes deleting custom SCSS, route files, and generated types, as well as modifying the Next.js configuration and Payload setup to streamline the project.

--- payload.config.ts
+++ payload.config.ts
@@ -1,37 +1,20 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-// storage-adapter-import-placeholder
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
-import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
-import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { buildConfig } from "payload";
-import sharp from "sharp";
-
-import { Media } from "./collections/Media";
-import { Users } from "./collections/Users";
-
-const filename = fileURLToPath(import.meta.url);
-const dirname = path.dirname(filename);
 
 export default buildConfig({
-	admin: {
-		user: Users.slug,
-		importMap: {
-			baseDir: path.resolve(dirname),
-		},
-	},
-	collections: [Users, Media],
-	editor: lexicalEditor(),
-	secret: process.env.PAYLOAD_SECRET || "",
-	typescript: {
-		outputFile: path.resolve(dirname, "payload-types.ts"),
-	},
+	secret: process.env.PAYLOAD_SECRET,
 	db: mongooseAdapter({
-		url: process.env.DATABASE_URI || "",
+		url: process.env.DATABASE_URI,
 	}),
-	sharp,
-	plugins: [
-		payloadCloudPlugin(),
-		// storage-adapter-placeholder
+	collections: [
+		{
+			slug: "pages",
+			fields: [
+				{
+					name: "title",
+					type: "text",
+				},
+			],
+		},
 	],
 });
